8b5ee6238a9d1b1e845314f630d92b695d96bbb9
Zac McClung <zac.mcclung370@gmail.com>
2018-05-10 12:22:58 -0600

    added spinner and conditional rendering

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
-import { Button, Card, CardSection, Input } from './common';
+import { Button, Card, CardSection, Input, Spinner } from './common';
 import firebase from 'firebase';
 
 class LoginForm extends Component {
-  state = { email: '', password: '', error: '' };
+  state = { email: '', password: '', error: '', loading: false };
 
   onButtonPress() {
     const { email, password } = this.state;
 
-    this.setState({ error: '' });
+    this.setState({ error: '', loading: true });
 
     firebase
       .auth()
@@ -17,6 +17,14 @@ class LoginForm extends Component {
       .catch(() => {
         this.setState({ error: 'Authentication Failed.' });
       });
+  }
+
+  renderButton() {
+    if (this.state.loading) {
+      return <Spinner size="small" />;
+    } else {
+      return <Button onPress={this.onButtonPress.bind(this)}>Login</Button>;
+    }
   }
 
   render() {
@@ -44,9 +52,7 @@ class LoginForm extends Component {
 
         <Text style={styles.errorTextStyle}>{this.state.error}</Text>
 
-        <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>Login</Button>
-        </CardSection>
+        <CardSection>{this.renderButton()}</CardSection>
       </Card>
     );
   }
