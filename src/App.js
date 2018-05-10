1359e10a4e463f27a04b538213b0ba19520e6cf7
Zac McClung <zac.mcclung370@gmail.com>
2018-05-09 23:02:36 -0600

    firebase client setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAL83QDAkmqUdGWh-K3QkRiMeCXIGRj9pU',
+      authDomain: 'authentication-40aae.firebaseapp.com',
+      databaseURL: 'https://authentication-40aae.firebaseio.com',
+      projectId: 'authentication-40aae',
+      storageBucket: 'authentication-40aae.appspot.com',
+      messagingSenderId: '888052558192'
+    });
+  }
   render() {
     return (
       <View>
